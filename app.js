44ac9ce72e030a19d456be21e53f86eddb72e3d8
Erdem Şen <developer@fykmobile.com>
2019-09-19 21:46:24 +0300

    cleaning up for final commit

--- app.js
+++ app.js
@@ -29,48 +29,60 @@ app.post('/getir', (req, res) => {
   //getting request body
   const reqData = req.body;
 
-  // getting desired records from collection
-  db.collection('records').aggregate([{
-      $match: {
-        createdAt: {
-          $gte: new Date(reqData.startDate),
-          $lt: new Date(reqData.endDate)
+  // checking for format of request
+  if (reqData.startDate && reqData.endDate && reqData.minCount && reqData.maxCount) {
+    
+    // getting desired records from collection
+    db.collection('records').aggregate([{
+        $match: {
+          createdAt: {
+            $gte: new Date(reqData.startDate),
+            $lt: new Date(reqData.endDate)
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          key: 1,
+          createdAt: 1,
+          totalCount: {
+            $sum: '$counts'
+          }
+        }
+      },
+      {
+        $match: {
+          totalCount: {
+            $gte: parseInt(reqData.minCount, 10),
+            $lt: parseInt(reqData.maxCount, 10)
+          }
         }
       }
-    },
-    {
-      $project: {
-        _id: 0,
-        key: 1,
-        createdAt: 1,
-        totalCount: {
-          $sum: '$counts'
-        }
+    ]).toArray((error, records) => {
+      if (error) {
+        res.status(400).send({
+          code: 1,
+          msg: 'DB connection error',
+          error
+        });
+      } else {
+        res.status(200).send({
+          code: 0,
+          msg: 'Success',
+          records
+        });
       }
-    },
-    {
-      $match: {
-        totalCount: {
-          $gte: parseInt(reqData.minCount, 10),
-          $lt: parseInt(reqData.maxCount, 10)
-        }
-      }
-    }
-  ]).toArray((error, records) => {
-    if (error) {
-      res.status(400).send({
-        msg: 'Error',
-        error
-      });
-    } else {
-      res.status(200).send({
-        code: 0,
-        msg: 'Success',
-        records
-      });
-    }
-  });
-})
+    });
+  } else {
+    res.status(400).send({
+      code: 2,
+      msg: 'Invalid format of response body'
+    });
+  }
+});
+
+// Setting port for both local and server
 var PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
